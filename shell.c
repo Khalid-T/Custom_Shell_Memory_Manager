1d585323e9d067746e40ace3ff4549f20f66222b
Khalid-T <khalidotewfik@gmail.com>
2026-09-16 17:42:29 -0400

    Pipes complete

--- shell.c
+++ shell.c
@@ -14,44 +14,79 @@ struct command {
     char *outfile;
 };
 
-void execute(struct command IO, int back_test) {
-    pid_t child = fork();
+void execute(struct command IO[], int back_test, int cmd_index) {
 
-    if (child == 0) {
-        if (IO.outfile != NULL) {
-            int out = open(IO.outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (out == -1) {
-                perror("errno");
-                exit(1);
+    int index = 0;
+    pid_t children[30];
+    int prev_read = -1;
+
+    while (index <= cmd_index) {
+
+        int pipefd[2];
+
+        if (pipe(pipefd) == -1) {
+            perror("Pipe Error");
+            exit(1);
+        }
+
+        children[index] = fork();
+
+        if (children[index] == 0) { // child process
+            if (prev_read != -1) {
+                dup2(prev_read, 0);
+                close(prev_read);
             }
-            dup2(out, 1);
-        }
-        if (IO.infile != NULL) {
-            int in = open(IO.infile, O_RDONLY);
-            if (in == -1) {
-                perror("errno");
-                exit(1);
+            if (index < cmd_index) {
+                dup2(pipefd[1], 1);
             }
-            dup2(in, 0);
+            close(pipefd[0]);
+            close(pipefd[1]);
+            if (IO[index].outfile != NULL) {
+                int out =
+                    open(IO[index].outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (out == -1) {
+                    perror("errno");
+                    exit(1);
+                }
+                dup2(out, 1);
+            }
+            if (IO[index].infile != NULL) {
+                int in = open(IO[index].infile, O_RDONLY);
+                if (in == -1) {
+                    perror("errno");
+                    exit(1);
+                }
+                dup2(in, 0);
+            }
+
+            execvp(IO[index].args[0], IO[index].args);
+
+            perror("execvp");
+            exit(1);
+        } else if (children[index] < 0) { // fork error
+            perror("fork");
+            exit(1);
         }
-        execvp(IO.args[0], IO.args);
-        perror("execvp");
-        exit(1);
-    } else if (child > 0) {
-        if (back_test == 0) {
-            waitpid(child, NULL, 0);
+        if (prev_read != -1) {
+            close(prev_read);
         }
-    } else if (child < 0) {
-        perror("fork");
+        close(pipefd[1]);
+        prev_read = pipefd[0];
+        index++;
     }
+    close(prev_read);
+    for (int i = 0; i < index; i++) {
+        if (back_test == 0) {
+            waitpid(children[i], NULL, 0);
+        }
+    };
 }
 
 int main() {
 
     char *buffer = NULL;
     size_t size = 0;
-    struct command IO;
-
+    struct command IO[16];
     while (1) {
 
         while (waitpid(-1, NULL, WNOHANG) > 0)
@@ -70,47 +105,57 @@ int main() {
         char *saveptr;
 
         char *token = strtok_r(buffer, " ", &saveptr);
+        int command_index = 0;
 
         int back_task = 0;
-        IO.infile = NULL;
-        IO.outfile = NULL;
+        IO[command_index].infile = NULL;
+        IO[command_index].outfile = NULL;
 
         while (token != NULL && index < 63) {
             if (strcmp(token, "&") == 0) {
                 back_task = 1;
             } else if (strcmp(token, "~") == 0) {
-                IO.args[index] = getenv("HOME");
+                IO[command_index].args[index] = getenv("HOME");
                 index++;
             } else if (strcmp(token, "<") == 0) {
                 token = strtok_r(NULL, " ", &saveptr);
-                IO.infile = token;
+                IO[command_index].infile = token;
             } else if (strcmp(token, ">") == 0) {
                 token = strtok_r(NULL, " ", &saveptr);
-                IO.outfile = token;
+                IO[command_index].outfile = token;
+            } else if (strcmp(token, "|") == 0) {
+                IO[command_index].args[index++] = NULL;
+                command_index++;
+
+                IO[command_index].infile = NULL;
+                IO[command_index].outfile = NULL;
+
+                index = 0;
 
             } else {
-                IO.args[index] = token;
+                IO[command_index].args[index] = token;
                 index++;
             }
             token = strtok_r(NULL, " ", &saveptr);
         }
-        IO.args[index] = NULL;
+        IO[command_index].args[index] = NULL;
 
-        if (IO.args[0] == NULL) {
+        if (IO[command_index].args[0] == NULL) {
             continue;
         }
 
-        if (strcmp(IO.args[0], "cd") == 0) {
-            if (IO.args[1] == NULL || strcmp(IO.args[1], "~") == 0) {
+        if (strcmp(IO[command_index].args[0], "cd") == 0) {
+            if (IO[command_index].args[1] == NULL ||
+                strcmp(IO[command_index].args[1], "~") == 0) {
                 chdir(getenv("HOME"));
-            } else if (chdir(IO.args[1]) != 0) {
+            } else if (chdir(IO[command_index].args[1]) != 0) {
                 perror("cd");
             }
 
             continue;
         }
 
-        execute(IO, back_task);
+        execute(IO, back_task, command_index);
     }
 
     free(buffer);
